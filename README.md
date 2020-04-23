974e5e583e7a45d31c2ba1e1343aa0bbb10ce9a7
Victor Sibanda <victormsibanda1@gmail.com>
2020-04-23 13:25:30 +0100

    Mostly Working

---
+++ README.md
@@ -0,0 +1,26 @@
+## Node Dev environment
+
+## To Start
+
+```
+git clone git@github.com:victorsibanda/NodeDevEnv.git
+```
+
+## To Use
+
+```
+vagrant up
+
+vagrant ssh app
+```
+
+## To Test and Use App
+
+```
+cd /home/ubuntu/app
+
+npm install
+npm test
+npm start
+
+```
--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -23,8 +23,14 @@ sudo npm install pm2 -g
 sudo apt-get install nginx -y
 
 # remove the old file and add our one
-sudo rm /etc/nginx/sites-available/default
-sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
+
+sudo systemctl start nginx
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/ubuntu/environment/reverse_proxy.conf /etc/nginx/sites-enabled/reverse_proxy.conf
+sleep 10
+sudo systemctl reload-or-restart nginx
+sleep 10
+sudo systemctl reload-or-restart nginx
 
 # finally, restart the nginx service so the new config takes hold
 sudo service nginx configtest
